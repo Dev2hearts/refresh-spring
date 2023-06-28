5664bea21b23095f04e0e181670cabe2bef1a812
노정민 <ssolen@naver.com>
2023-06-28 18:14:07 +0900

    User - jungmin

--- src/main/java/com/green/refresh/user/UserController.java
+++ src/main/java/com/green/refresh/user/UserController.java
@@ -4,6 +4,7 @@ import com.green.refresh.user.model.UserInsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class UserController {
     }
 
     @PostMapping
-    public int postUser(UserInsDto dto) {
+    public int postUser(@RequestBody UserInsDto dto) {
         return service.insUser(dto);
     }
 }
