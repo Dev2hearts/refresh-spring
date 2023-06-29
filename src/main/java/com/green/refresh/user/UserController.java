5d93e7084a909649942d2266b6928a829a3fe9ef
노정민 <ssolen@naver.com>
2023-06-29 15:22:46 +0900

    UserController - jungmin

--- src/main/java/com/green/refresh/user/UserController.java
+++ src/main/java/com/green/refresh/user/UserController.java
@@ -1,12 +1,10 @@
 package com.green.refresh.user;
 
 import com.green.refresh.user.model.UserInsDto;
+import com.green.refresh.user.model.UserUpdDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/refresh/user")
@@ -16,7 +14,23 @@ public class UserController {
 
 
     @PostMapping
+    @Operation(summary = "회원가입", description = "" +
+            "nm: [10] 유저 닉네임,<br>" +
+            "gender: [1] 성별(M: 남성, F: 여성),<br>" +
+            "birth [8] 생년월일,<br>")
     public int postUser(@RequestBody UserInsDto dto) {
         return service.insUser(dto);
+    }
+
+
+
+    @PatchMapping
+    public int UpdInfoUser(@RequestBody UserUpdDto dto) {
+        return service.updUser(dto);
+    }
+
+    @DeleteMapping
+    public int deleteUser(@RequestParam int iuser) {
+        return service.delUser(iuser);
     }
 }
--- src/main/java/com/green/refresh/user/UserMapper.java
+++ src/main/java/com/green/refresh/user/UserMapper.java
@@ -1,9 +1,13 @@
 package com.green.refresh.user;
 
+import com.green.refresh.user.model.UserDelDto;
 import com.green.refresh.user.model.UserInsDto;
+import com.green.refresh.user.model.UserUpdDto;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
     int insUser(UserInsDto dto);
+    int updUser(UserUpdDto dto);
+    int delUser(UserDelDto dto);
 }
