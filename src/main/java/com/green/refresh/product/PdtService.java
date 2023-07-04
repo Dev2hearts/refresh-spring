db8776f8beb01f4dbe9ca96230ad9c37d7313bfe
장하은 <haun0@naver.com>
2023-07-04 16:59:37 +0900

    haeun - Pdt code정리

--- src/main/java/com/green/refresh/product/PdtService.java
+++ src/main/java/com/green/refresh/product/PdtService.java
@@ -12,39 +12,18 @@ public class PdtService {
     private final PdtMapper mapper;
 
     public int insPdt(PdtInsDto dto) {
-
-/*        PdtEntity entity = new PdtEntity();
-        entity.setIplan(iplan);
-        entity.setIcate(dto.getIcate());
-        entity.setIunit(dto.getIunit());
-        entity.setBIuser(dto.getWIuser());
-        entity.setNm(dto.getNm());*/
-
-
-/*        int result = mapper.insPdt(entity);
-        if (result == 1){
-            return entity.getIproduct();
-        }*/
-
         return mapper.insPdt(dto);
-
     }
 
     public List<PdtSelVo> selPdt(int igroup, int iplan) {
         PdtSelDto dto = new PdtSelDto();
         dto.setIgroup(igroup);
         dto.setIplan(iplan);
-//        return mapper.selPdt(dto);
         return mapper.selPdt(dto);
     }
 
     public int updPdt(PdtUpdDto dto) {
-/*        PdtEntity entity = new PdtEntity();
-        entity.setNm(dto.getNm());
-        entity.setCnt(dto.getCnt());
-        entity.setIcate(dto.getIcate());
-        entity.setIunit(dto.getIunit());
-        entity.setIproduct(dto.getIproduct());*/
+
         return mapper.updPdt(dto);
     }
 
