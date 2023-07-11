a538dc81a8505a2f308fc3e5f869c912edc0dd29
장하은 <haun0@naver.com>
2023-07-11 10:34:22 +0900

    Pdt sel 수정 - haeun

--- src/main/java/com/green/refresh/product/model/PdtSelVo.java
+++ src/main/java/com/green/refresh/product/model/PdtSelVo.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class PdtSelVo {
     private int iproduct;
+    private int icate;
+    private int iunit;
     private String cateNm;
     private String nm;
     private int cnt;
